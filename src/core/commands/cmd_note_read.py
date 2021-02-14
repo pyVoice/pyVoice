74dbac1e36ead29ec375f64251112e808b2a32fc
Afonso Santos <afonso.faisca1@gmail.com>
2021-02-14 17:21:46 +0000

    Added notes read command

---
+++ src/core/commands/cmd_note_read.py
@@ -0,0 +1,18 @@
+import json
+import time
+
+from src.core.modules import tts, replying
+
+
+def ex(cmd):
+    with open("data/files/json/notes.json", "r+", encoding="utf-8") as notes_file:
+        notes_file_data = json.load(notes_file)
+
+    tts.speak(replying.get_reply("note_read"))
+
+    if len(notes_file_data["notes"]) == 0:
+        tts.speak(replying.get_reply("notes_read", stage=1))
+    else:
+        for note in notes_file_data["notes"]:
+            tts.speak(note)
+            time.sleep(0.5)
