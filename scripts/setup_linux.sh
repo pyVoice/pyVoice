73534fad1867972941cca070d6a06c19b4a71f77
Afonso Santos <afonso.faisca1@gmail.com>
2021-03-01 19:44:01 +0000

    Added Linux spec and improved script

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo "\n\n Setup pyAudio... \n\n"
+echo "Setup pyAudio..."
 sudo apt install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 
-echo "\n\n Setup Python 3.9 dev... \n\n"
+echo "Setup Python 3.9 dev..."
 sudo apt install python3.9-dev
 
-echo "\n\n Setup PIP packages \n\n"
+echo "Setup PIP packages"
 pip install -U setuptools wheel
 pip install vext
 pip install vext.gi
+
+echo "Setup cairo..."
+sudo apt-get install libcairo2-dev
+
+echo "Setup random stuff..."
+sudo apt install libgirepository1.0-dev
+sudo apt install python3-gst-1.0
+
+echo "Setup PyGObject"
+pip install PyGObject
