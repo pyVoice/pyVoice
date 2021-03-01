7dc181abe856130d7576dea2077c527e87ce1783
Afonso Santos <afonso.faisca1@gmail.com>
2021-03-01 19:05:25 +0000

    Improved Linux setup script

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Setup pyAudio..."
+echo "\n\n Setup pyAudio... \n\n"
 sudo apt install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 
-echo "Setup Python 3.9 dev..."
+echo "\n\n Setup Python 3.9 dev... \n\n"
 sudo apt install python3.9-dev
 
-echo "Setup Vext and Gi"
+echo "\n\n Setup PIP packages \n\n"
 pip install -U setuptools wheel
 pip install vext
 pip install vext.gi
