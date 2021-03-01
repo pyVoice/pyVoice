97fd713273ff3f4518155773007fe7490bdbbb69
Afonso Santos <afonso.faisca1@gmail.com>
2021-03-01 16:26:25 +0000

    Added Linux setup script

---
+++ scripts/setup_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Setup pyAudio..."
+sudo apt install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+
+echo "Setup Python 3.9 dev..."
+sudo apt install python3.9-dev
+
+echo "Setup Vext and Gi"
+pip install vext
+pip install vext.gi
